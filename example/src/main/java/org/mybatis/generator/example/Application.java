d38cdc6ffbb6929a5a54168176b310fa08b00fa3
i-zacky <aizaki.takayuki@gmail.com>
2023-04-17 02:05:12 +0900

    exampleの起動調整

---
+++ example/src/main/java/org/mybatis/generator/example/Application.java
@@ -0,0 +1,16 @@
+package org.mybatis.generator.example;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    new SpringApplicationBuilder(Application.class) //
+        .bannerMode(Banner.Mode.OFF) //
+        .build() //
+        .run(args);
+  }
+}
---
+++ example/src/main/java/org/mybatis/generator/example/properties/DataSourceProperties.java
@@ -0,0 +1,34 @@
+package org.mybatis.generator.example.properties;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class DataSourceProperties {
+
+  String driverClassName;
+
+  String poolName;
+
+  String url;
+
+  String username;
+
+  String password;
+
+  int maximumPoolSize;
+
+  int minimumIdle;
+
+  int idleTimeout;
+
+  int connectionTimeout;
+
+  long leakDetectionThreshold;
+}
